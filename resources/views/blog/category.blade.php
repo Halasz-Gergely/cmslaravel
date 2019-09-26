33761ec7958e202b5443ac858a0b7005d837aad1
G3Ri <hgeri011@gmail.com>
2019-09-26 18:49:34 +0200

    Tags Categories functionality combined with search. If searched within category, it will give back only the category related posts.

--- resources/views/blog/category.blade.php
+++ resources/views/blog/category.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.blog')
 @section('title')
-TheSaaS — Blog with sidebar
+    {{ $category->name }}
 @endsection
 @section('header')
-<header class="header text-center text-white" style="background-image: linear-gradient(-225deg, #5D9FFF 0%, #B8DCFF 48%, #6BBBFF 100%);">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h1>Latest Blog Posts</h1>
-                <p class="lead-2 opacity-90 mt-6">Read and get updated on how we progress</p>
+    <header class="header text-center text-white" style="background-image: linear-gradient(-225deg, #5D9FFF 0%, #B8DCFF 48%, #6BBBFF 100%);">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h1>{{ $category->name }}</h1>
+                    <p class="lead-2 opacity-90 mt-6">Read and get updated on how we progress</p>
+                </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
 @endsection
 @section('content')
-<main class="main-content">
+    <main class="main-content">
         <div class="section bg-gray">
             <div class="container">
                 <div class="row">
@@ -43,3 +43,4 @@ TheSaaS — Blog with sidebar
         </div>
     </main>
 @endsection
+
